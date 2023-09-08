c73512344e08f9833ee21e997dc5658494623d63
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-09-08 11:14:02 +0200

    sorting todos before getting them

--- 13-firebase-todos/src/hooks/useGetTodos.ts
+++ 13-firebase-todos/src/hooks/useGetTodos.ts
@@ -1,11 +1,12 @@
 
+import { orderBy } from 'firebase/firestore'
 import { todosCol } from '../services/firebase'
 import { Todo } from '../types/Todo.types'
 import useStreamCollection from './useStreamCollection'
 
 const useGetTodos = () => {
 	//call generic hook to get collection
-	return useStreamCollection<Todo>(todosCol)
+	return useStreamCollection<Todo>(todosCol, orderBy('title'))
 
 }
 
--- 13-firebase-todos/src/hooks/useStreamCollection.ts
+++ 13-firebase-todos/src/hooks/useStreamCollection.ts
@@ -1,14 +1,21 @@
-import { CollectionReference, onSnapshot } from 'firebase/firestore'
+import { CollectionReference, QueryConstraint, onSnapshot, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
-const useStreamCollection = <T>(colRef: CollectionReference<T>) => {
+const useStreamCollection = <T>(
+	colRef: CollectionReference<T>,
+	...queryConstraints: QueryConstraint[]
+) => {
 	const [data, setData] = useState<T[] | null>(null)
 	const [loading, setLoading] = useState(true)
 
+
 	// Get data on component mount
 	useEffect(() => {
+		// Construct a query reference
+		const queryRef = query(colRef, ...queryConstraints)
+
 		// Subscribe to changes in the collection
-		const unsubscribe = onSnapshot(colRef, (snapshot) => {
+		const unsubscribe = onSnapshot(queryRef, (snapshot) => {
 			// loop over all docs
 			const data: T[] = snapshot.docs.map(doc => {
 				return {
